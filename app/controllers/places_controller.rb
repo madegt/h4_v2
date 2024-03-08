a09afd87c64d07a494ef8baaaae99331c3451912
Madeleine Garbin <madeleine.garbin@kellogg.northwestern.edu>
2024-03-08 23:44:37 +0000

    controllers

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,7 +6,9 @@ class PlacesController < ApplicationController
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
-    @entries = Entry.where({ "place_id" => @place["id"] })
+    @entries = Entry.where({ "place_id" => @place["id"] , "user_id" => @current_user["id"]})
+    @place = Place.find(params[:id])
+    @is_place_profile = true
   end
 
   def new
@@ -20,3 +22,4 @@ class PlacesController < ApplicationController
   end
 
 end
+
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new
-    @user["first_name"] = params["first_name"]
-    @user["last_name"] = params["last_name"]
-    @user["email"] = params["email"]
-    @user["password"] = params["password"]
+    @user["username"] = params["username"]
+      @user["email"] = params["email"]
+    @user["password"] = BCrypt::Password.create(params["password"])
     @user.save
-    redirect_to "/"
+    flash[:notice] = "Thanks for signing up. Now login."
+    redirect_to "/login"
+
   end
 end
