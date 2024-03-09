34be40ea1e9b547963c437f90e639fe57a9790df
Madeleine Garbin <madeleine.garbin@kellogg.northwestern.edu>
2024-03-09 00:07:57 +0000

    upload

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,8 @@
 class Entry < ApplicationRecord
+  class Entry < ApplicationRecord
+    belongs_to :place
+    belongs_to :user
+  
+    has_one_attached :uploaded_image
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,15 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+# config/environments/development.rb
+Rails.application.configure do
+  # Other configuration options...
+
+  # Specify Active Storage service
+  config.active_storage.service = :local
+end
+
+
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
